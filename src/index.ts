4ed8696727a1ecbfd47ec0738dad0eb34000dade
罗智毅 <luozhiyi@yaowutech.com>
2023-06-20 22:58:30 +0800

    chore: 优化color 导出

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import '@leslies/css/dist/esm/default.css';
 export { createColorData, createColorData_v2 } from './utils';
 
 export { default as Button } from './components/button';
-export { default as Palette } from './components/palette';
-export { Color } from './components/palette';
+export { default as Palette, Color } from './components/palette';
 export { default as Space } from './components/space';
